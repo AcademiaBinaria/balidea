0ce1a7ef3a463172e97de726b2ac9090018f536e
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:49:14 +0200

    feat(main) : router-outlet and RouterModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
@@ -7,7 +8,7 @@ import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [ShellComponent]
 })
 export class CoreModule {}
