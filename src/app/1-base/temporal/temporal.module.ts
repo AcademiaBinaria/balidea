929fb8678e22e13e007a2d32ab3910781aebe189
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:55:54 +0200

    feat(base): ng g c 1-base/temporal --export=true

--- src/app/1-base/temporal/temporal.module.ts
+++ src/app/1-base/temporal/temporal.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TemporalComponent } from './temporal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TemporalComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TemporalComponent]
 })
 export class TemporalModule { }
