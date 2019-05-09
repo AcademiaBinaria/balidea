27fb0fae673f603eae4f2125172c157d3c506bcf
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:45:03 +0200

    feat(core): generate components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell/shell.component';
+import { HeaderComponent } from './shell/header/header.component';
+import { MainComponent } from './shell/main/main.component';
+import { FooterComponent } from './shell/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
   imports: [
     CommonModule
   ]
