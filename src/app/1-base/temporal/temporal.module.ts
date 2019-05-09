34afc90a1874879e0db0058ac7d1f98dd4a22f9f
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:57:03 +0200

    feat(temporal): use app-now from SharedModule

--- src/app/1-base/temporal/temporal.module.ts
+++ src/app/1-base/temporal/temporal.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 import { TemporalComponent } from './temporal.component';
 
 @NgModule({
   declarations: [TemporalComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, SharedModule],
   exports: [TemporalComponent]
 })
-export class TemporalModule { }
+export class TemporalModule {}
