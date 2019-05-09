1a6e5378ba3bc25d070e51e514a0dd68e5ede5c1
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:46:51 +0200

    feat(shell): crerate and use shell ina app

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShellComponent } from './shell/shell.component';
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
-import { FooterComponent } from './shell/footer/footer.component';
+import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [ShellComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
