a4826c712c837992828859d3e3c3288b413a1ae3
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 11:48:05 +0200

    feat(header): title from appp to header

--- src/app/core/shell/header/header.component.ts
+++ src/app/core/shell/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HeaderComponent implements OnInit {
+  public title = environment.appName + 'hello world ;-)';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
