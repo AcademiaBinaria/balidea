ab3bdbe042cb794d8c1ac5ea6e81e9b3ceb08120
Alberto Basalo <albertobasalo@hotmail.com>
2019-05-09 12:00:27 +0200

    feat(temporal): temporal component in main

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TemporalModule } from '../1-base/temporal/temporal.module';
 import { FooterComponent } from './shell/footer/footer.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { MainComponent } from './shell/main/main.component';
@@ -8,7 +9,7 @@ import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
   declarations: [ShellComponent, HeaderComponent, MainComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TemporalModule],
   exports: [ShellComponent]
 })
 export class CoreModule {}
